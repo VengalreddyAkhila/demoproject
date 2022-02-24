94c54857627b5529c6807b4903d5014cc2845d14
VengalreddyAkhila <akhilv@bipp.io>
2022-02-24 15:35:31 +0530

    [deleted file]

--- index.js
+++ index.js
@@ -134,7 +134,10 @@ app.get("/dev/newj", (req, res) => {
  
 });
 
-app.get("/dev/demo1", (req, res) => {
+
+
+
+app.get("/", (req, res) => {
 
   res.send(`<!DOCTYPE html>
   <html>
@@ -149,23 +152,26 @@ app.get("/dev/demo1", (req, res) => {
       </head>
   <body>
       <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-      <script src="https://unpkg.com/bipp-dev@1.0.6/bipp.js"></script>
+      <script src="https://unpkg.com/bipp"></script>
   
   <div class="main">
-    <h2>Bipp dashboard</h2>
+    <h2>Company Website</h2>
+    <h2>Retail Sales Dashboard</h2>
     <div class="dashboard">
         <div id='container'></div>
     </div>
   </div>
   <script>
 
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
 
-      let url = "https://xkmzkx.app.bippanalytics.com/embed/41fc8586-f7bf-453c-9636-bb198fe597a5?id=1b89c314-5365-4d72-bf36-f64e825d0a46&cid=ffc632e8723641ff9346caf70dea94b6.xkmzkx.app.bippanalytics.com&secret=0llGdlhUyoLm1!LBxPc6wWi5u2L4kWrCVSstH4bwNo0Q5yyy";
+      //let url = "https://ketaki-desale.app.bipp.io/embed/68cc77a6-fc72-497d-bbb9-0b1ffe01e579?id=707b84b7-7a5c-47a5-9847-a163319daa95&cid=f570dc6573a6478cb954e82fa603ae97.ketaki-desale.app.bipp.io&secret=MNTGHrqSnWBQ0zonnyfphbJn3!mFM!@RP!davQJi!lXkUFEz";
+      //let url = "https://ketaki-desale.app.bipp.io/embed/443c2551-937f-44c6-8252-88f3d4739ebc?id=a8c75ef8-d90a-404f-a5de-5139952513ab&cid=16cc46c2c9054fdeac09ea1f6385b380.ketaki-desale.app.bipp.io&secret=r6hXP@nwPUmDFeRO9R@b25dyrfTIv65mOsVPPKEtPJRQncPF";    
+      let url = "https://ketaki-desale.app.bipp.io/embed/434560d5-d81e-4ac1-8953-879609d17217?id=6e0f6e85-c259-4634-abce-ca109bfc60ff&cid=aa6a7b1ddeb44c0b83de98c33c65e1ea.ketaki-desale.app.bipp.io&secret=x1kYW1MRpp0c9YfHzAEctxhtgGpxJhAFAof9i5LJesvsK@bc"
       let config = { id : 'container', width: "100%", height: height, style: 'border:none;' }
       
-      let bipp = new Bipp({debug: 1});
+      let bipp = new Bipp();
       bipp.load(url, config);
   </script>
   
@@ -173,8 +179,6 @@ app.get("/dev/demo1", (req, res) => {
   </html>`)
  
 });
-
-
 
 app.listen(port, function() {
   console.log("Running node server", "on port " + port);
