08ab34d8f32d40aa5370947ebc0f518fb68d4187
VengalreddyAkhila <akhilv@bipp.io>
2022-02-24 12:26:56 +0530

    [modified :code]

--- app.js
+++ app.js
@@ -2,8 +2,44 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-  res.send("Hello world");
+
+
+app.get("/dev/exp2", (req, res) => {
+
+  res.send(`<!DOCTYPE html>
+  <html>
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <style>
+            .main {
+                margin: 30px;
+            }
+    
+        </style>
+      </head>
+  <body>
+
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="https://unpkg.com/bipp"></script>
+
+  <div id="main">Call Response</div>
+
+  <script>
+
+  let element = document.getElementById('main');
+  axios.get('https://harisha.herokuapp.com/test')
+  .then(response => {
+      console.log(response.data);
+      element.innerHTML = response.data;
+  })
+            
+  </script>
+  <iframe src="https://harisha.herokuapp.com">
+  </iframe>
+  
+  </body>
+  </html>`)
+ 
 });
 
 app.listen(PORT, () =>{
