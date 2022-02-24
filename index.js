14731dcebd2b63481438084c000756ef6f891562
VengalreddyAkhila <akhilv@bipp.io>
2022-02-24 15:42:41 +0530

    [modified]

--- index.js
+++ index.js
@@ -13,43 +13,7 @@ app.use(bodyParser.json());
 
 let port = process.env.PORT || 9000;
 
-app.get("/dev/exp2", (req, res) => {
 
-    res.send(`<!DOCTYPE html>
-    <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <style>
-              .main {
-                  margin: 30px;
-              }
-      
-          </style>
-        </head>
-    <body>
-
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script src="https://unpkg.com/bipp"></script>
-
-    <div id="main">Call Response</div>
-
-    <script>
-
-    let element = document.getElementById('main');
-    axios.get('https://harisha.herokuapp.com/test')
-    .then(response => {
-        console.log(response.data);
-        element.innerHTML = response.data;
-    })
-              
-    </script>
-    <iframe src="https://harisha.herokuapp.com">
-    </iframe>
-    
-    </body>
-    </html>`)
-   
-  });
 
 app.get("/", (req, res) => {
 
