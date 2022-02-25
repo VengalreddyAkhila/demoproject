a439aee9cf4ef5e67d1f89223ea44064537563b8
VengalreddyAkhila <akhilv@bipp.io>
2022-02-25 10:58:14 +0530

    [modified]

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ app.get("/", (req, res) => {
   <div class="main">
   <h1>Bipp Platform</h1>
   <h2>Dashboard</h2>
-
     <div class="dashboard">
         <div id='container'></div>
     </div>
@@ -44,7 +43,7 @@ app.get("/", (req, res) => {
       const height = window.innerHeight;
 
        let url = "https://ketaki-desale.app.bipp.io/embed/434560d5-d81e-4ac1-8953-879609d17217?id=6e0f6e85-c259-4634-abce-ca109bfc60ff&cid=aa6a7b1ddeb44c0b83de98c33c65e1ea.ketaki-desale.app.bipp.io&secret=x1kYW1MRpp0c9YfHzAEctxhtgGpxJhAFAof9i5LJesvsK@bc"
-      let config = { id : 'container', width: "100%", height: height, style: 'border:none;' }
+      let config = { id : 'container', width: "90%", height: height, style: 'border:none;' }
       
       let bipp = new Bipp();
       bipp.load(url, config);
